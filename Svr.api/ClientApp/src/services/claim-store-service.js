c75f2070aed31262e48879fab045c21cf286aaeb
romashka_77 <romashka_77@mail.ru>
2019-05-23 17:19:09 +0300

    1

--- Svr.api/ClientApp/src/services/claim-store-service.js
+++ Svr.api/ClientApp/src/services/claim-store-service.js
@@ -14,6 +14,56 @@ export default class ClaimStoreService
         title: 'Release It!',
         author: 'Michael T. Nygard'
       },
+      {
+        id: 3,
+        title: 'Production-Ready MicroServices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 4,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      },
+      {
+        id: 5,
+        title: 'Production-Ready MicroServices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 6,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      },
+      {
+        id: 7,
+        title: 'Production-Ready MicroServices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 8,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      },
+      {
+        id: 9,
+        title: 'Production-Ready MicroServices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 10,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      },
+      {
+        id: 11,
+        title: 'Production-Ready MicroServices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 12,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      },
     ];
   }
 
